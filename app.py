d52b9f43ca38f54c6035f64b3b64b15880954eef
MIGUEL ANGEL GARATEJO <70038701+miguelcapera2011@users.noreply.github.com>
2026-05-16 07:46:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -145,6 +145,23 @@ h2, h3 {
 # =========================
 
 st.markdown("""
+
+<div class="topbar">
+
+    <img class="logo"
+    src="https://cdn-icons-png.flaticon.com/512/4149/4149670.png">
+
+    <div>
+        <p class="titulo-principal">
+            Minería de Datos - KMeans
+        </p>
+
+        <p class="subtitulo">
+            Análisis interactivo y visualización avanzada de clustering
+        </p>
+    </div>
+
+</div>
 Esta aplicación permite explorar paso a paso el algoritmo K-Means usando el dataset USArrests.
 
 Incluye:
